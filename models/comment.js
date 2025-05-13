870e4991b32f3c981e2727b5f51da23e2fd1e57c
Rory Swietlicki <rory@swietlicki.eu>
2025-05-13 14:22:42 +0100

    Added movie and comment model

---
+++ models/comment.js
@@ -0,0 +1,24 @@
+import mongoose from 'mngoose'
+
+const commentSchema = new mongoose.Schema({
+    content: {
+        type: String,
+        required: true
+    },
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    movie: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Movie',
+        required: true
+    }
+}, {
+    timestamp: true
+})
+
+const Comment = mongoose.model('Comment', commentSchema)
+
+export default Comment
---
+++ models/movie.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose'
+
+const movieSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Oops, you forgot to provide your movie title!']
+    },
+    director: {
+        type: String,
+        required: [true, 'Oops, you forgot to provide a director!']
+    },
+    runTime: {
+        type: Number,
+        required: [true, 'Oops, you forgot to provide a run time!']
+    },
+    tags: {
+        type: [String],
+    },
+    owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    movieImage: {
+        type: String,
+        required: [true, 'Oops, you forgot to provide a movie image!']
+    }
+})
+
+const Movie = mongoose.model('Movie', movieSchema)
+
+export default Movie
+
