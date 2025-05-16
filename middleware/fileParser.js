08f02abea32709798c31c1d98d033217a597aab5
Rory Swietlicki <rory@swietlicki.eu>
2025-05-16 16:09:08 +0100

    Fixed file parser

--- middleware/fileParser.js
+++ middleware/fileParser.js
@@ -12,7 +12,7 @@ cloudinary.config({
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
-        folder: Cheapflix_uploads,
+        folder: 'Cheapflix_uploads',
         format: ['png', 'jpeg', 'JPEG', 'jpg', 'webp'],
         public_id: (req, file) => {
             return Date.now() + '-' + file.originalname.split('.')[0]
